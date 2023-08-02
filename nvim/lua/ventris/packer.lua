269003a4c4f50f28c93e315dcdbedefae6e62ed6
ventris <ss@tinbox.nu>
2023-08-02 11:51:19 +0200

    Adding new neovim plugins

--- nvim/lua/ventris/packer.lua
+++ nvim/lua/ventris/packer.lua
@@ -4,43 +4,78 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use ({
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  })
-  use ({
-	  "catppuccin/nvim",
-	  as = "catppuccin",
-  })
-  use (
-	  'nvim-treesitter/nvim-treesitter',
-	  {run = ':TSUpdate'}
-  )
-  use ( 'theprimeagen/harpoon' )
-  use ( 'mbbill/undotree' )
-  use ( 'tpope/vim-fugitive' )
-  use ( 'nvim-treesitter/nvim-treesitter-context' );
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.api.nvim_command, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use ({
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    })
+    use ({
+        "catppuccin/nvim",
+        as = "catppuccin",
+    })
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
 
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-  }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    use("theprimeagen/refactoring.nvim")
+    use ( 'theprimeagen/harpoon' )
+    use ( 'mbbill/undotree' )
+    use ( 'tpope/vim-fugitive' )
+    use ( 'nvim-treesitter/nvim-treesitter-context' )
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.api.nvim_command, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+    use("folke/zen-mode.nvim")
+    use("github/copilot.vim")
+    use("laytan/cloak.nvim")
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+    use({
+        'Wansmer/treesj',
+        requires = { 'nvim-treesitter' },
+        config = function()
+            require('treesj').setup({--[[ your config ]]})
+        end,
+    })
 end)
